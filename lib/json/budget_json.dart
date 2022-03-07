98dda556467ba43a9d6eb770df49411ebbd7afd5
vaibh <vaibhavdudeperfect@gmail.com>
2022-03-07 10:51:07 +0530

    Adding Budget json file

---
+++ lib/json/budget_json.dart
@@ -0,0 +1,26 @@
+import 'package:budget_tracker_ui/theme/colors.dart';
+import 'package:flutter/material.dart';
+
+const List budget_json = [
+  {
+    "name": "Gift",
+    "price": "\Rs.2250.00",
+    "label_percentage": "45%",
+    "percentage": 0.45,
+    "color": Colors.greenAccent
+  },
+  {
+    "name": "Automobile",
+    "price": "\Rs.300000.00",
+    "label_percentage": "90%",
+    "percentage": 0.7,
+    "color": red
+  },
+  {
+    "name": "Bank",
+    "price": "\Rs.400000.00",
+    "label_percentage": "90%",
+    "percentage": 0.9,
+    "color": blue
+  }
+];
