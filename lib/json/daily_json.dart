75b5286ab3c1bc85bbc1b5518be14f1410568b51
vaibh <vaibhavdudeperfect@gmail.com>
2022-03-07 13:01:07 +0530

    Adding daily needs page

---
+++ lib/json/daily_json.dart
@@ -0,0 +1,32 @@
+const List daily = [
+  {
+    "icon": "assets/images/bank.png",
+    "name": "Bank",
+    "date": "Fri 10AM",
+    "price": "\$340.00"
+  },
+  {
+    "icon": "assets/images/auto.png",
+    "name": "Automobile",
+    "date": "Mon 8AM",
+    "price": "\Rs.70.00"
+  },
+  {
+    "icon": "assets/images/gift.png",
+    "name": "Gift",
+    "date": "Sat 6PM",
+    "price": "\Rs.110.00"
+  },
+  {
+    "icon": "assets/images/eating.png",
+    "name": "Eating",
+    "date": "Sun 5PM",
+    "price": "\Rs.60.00"
+  },
+  {
+    "icon": "assets/images/charity.png",
+    "name": "Charity",
+    "date": "Wed 12PM",
+    "price": "\Rs.1200.00"
+  }
+];
